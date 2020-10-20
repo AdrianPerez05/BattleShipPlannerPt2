13c1339d0ad32f49c09df7c7fe34cbcf8c7c3919
adrianperez <adrianperez@stu.postoakschool.org>
2020-10-20 00:23:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+'''
+Adrian Perez
+BattleShip Planner
+October 20th 2020
+'''
+
+def Introduction():
+    #Welcome To Battleship
+def numberfplayer():
+    #Player 1 or 2
+def didIwin():
+    #at the end of every round, did i win message
+def movecol():
+    #chose what spot to attack at or bomb
+def sinkship():
+    #message to show a ship has sunk
+def hitormiss():
+    #message stating if you hit or missed the battle ship 
+
